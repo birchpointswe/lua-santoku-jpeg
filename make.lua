de072a10a21d930ffd1478211ecd22894f7bf82e
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-18 13:39:26 -0400

    bump to 0.0.20-1, update license and deps

--- make.lua
+++ make.lua
@@ -1,15 +1,15 @@
 local env = {
 
   name = "santoku-jpeg",
-  version = "0.0.19-1",
+  version = "0.0.20-1",
   variable_prefix = "TK_JPEG",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.204-1",
-    "santoku-fs >= 0.0.32-1"
+    "santoku >= 0.0.328-1",
+    "santoku-fs >= 0.0.45-1"
   },
 
   test = {
@@ -19,7 +19,7 @@ local env = {
     wasm = {
       ldflags = { "--bind" },
       dependencies = {
-        "santoku-web >= 0.0.100-1"
+        "santoku-web >= 0.0.498-1"
       }
     }
   },
